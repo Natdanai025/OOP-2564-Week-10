f0e5c7b1c3e443a3dbfbc85997c732361ff4258c
Natdanai Wisetkulprom <92078990+Natdanai025@users.noreply.github.com>
2022-05-18 15:02:21 +0700

    63030025

---
+++ 63030025/project 10.1/project 10.1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace method_examples
+{
+    class Student
+    {
+        int id;                 // instance member
+        static string     ;    // static member
+        internal void SetId(int value)
+        {
+            id = value;
+            ShowId();
+        }
+        internal void Set(string value)
+        {
+                 = value;
+            Show();
+        }
+
+        internal void ShowId()
+        {
+            Console.WriteLine($"id : hashcode = [{this.id.GetHashCode():X}], value = {id}");
+        }
+
+        internal unsafe void Show()
+        {
+            Console.WriteLine($"     : hashcode = [{    .GetHashCode():X}], value = {    }");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            //  สร้าง instance "s1"
+            Student s1 = new Student();
+            Console.WriteLine($"Instance 's1' Hashcode = {s1.GetHashCode():X8}");
+
+            //  สร้าง instance "s2"
+            Student s2 = new Student();
+            Console.WriteLine($"Instance 's2' Hashcode = {s2.GetHashCode():X8}");
+
+            //  สร้าง instance "s3"
+            Student s3 = new Student();
+            Console.WriteLine($"Instance 's3' Hashcode = {s3.GetHashCode():X8}");
+            Console.WriteLine();
+
+            //  กำหนดและแสดงค่าใน member ของ s1
+            s1.SetId(1001);
+            s1.Set("Computer Engineering");
+            Console.WriteLine();
+
+            //  กำหนดและแสดงค่าใน member ของ s2
+            s2.SetId(1002);
+            s2.Set("Electrical Engineering");
+            Console.WriteLine();
+
+            //  กำหนดและแสดงค่าใน member ของ s3
+            s3.SetId(1003);
+            s3.Set("Mechanical Engineering");
+            Console.WriteLine();
+
+            //  แสดงค่าใน static member ของ s1-s3 อีกครั้ง
+            Console.Write("S1."); s1.Show();
+            Console.Write("S2."); s2.Show();
+            Console.Write("S3."); s3.Show();
+        }
+    }
+}
